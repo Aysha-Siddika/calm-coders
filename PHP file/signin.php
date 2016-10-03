7b95d22f2bddcca5f3f079ab81ed8466fc007d26
Aysha-Siddika <aysha.sami96@gmail.com>
2016-10-03 09:47:49 -0700

    3rd update

--- PHP file/signin.php
+++ PHP file/signin.php
@@ -29,46 +29,8 @@ if(!empty($_POST['pass']))
 	{	echo "<script type=\"text/javascript\">window.alert('Invalid User!');</script>";
 		
 	}
+mysqli_close($conn);
 }
 
 
 ?>
-
-<html>
-    <head>
-    <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    </head>
-<body>
-    <div class="container">
-        <div class="col-lg-4 col-md-4">
-        
-        </div>
-           <div class="col-lg-4 col-md-4 well" style="margin-top:20%;">
-            <form action="" class="form" method="post">
-    <div class="form-group">
-      <input type="text" name="un" placeholder="Username" class="form-control" required /><br/>
-    </div>
-     <div class="form-group">
-    <input type="password" name="pass" placeholder="Password" class="form-control" required/><br/>
-    </div>
-     <div class="form-group">
-     <input type="submit" name="submit" class="btn btn-primary pull-right" value="submit"/> 
-        </div>
-</form>
-    </div>
-  
-    
-   
- 
-    </div>
-   
-</body>
-
-</html>
--- PHP file/signup.php
+++ PHP file/signup.php
@@ -19,77 +19,19 @@ if(!empty($_POST['pass']))
     
   
  $sql = "INSERT INTO user_details1 (fullname, username,password,confirm_password,emergency_no1,emergency_no2,emergency_no3,emergency_no4)
-VALUES ( '$fname','$uname','$enpass','$enpass', '$en1l','$en2','$en3','$en4')";
+VALUES ( '$fname','$uname','$enpass','$enpass', '$en1','$en2','$en3','$en4')";
     
     if (mysqli_query($conn, $sql)) {
     echo "successfully account created";
 
-} else {
+}
+ else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
+mysqli_close($conn);
 }
     
   
 
 ?>
-    <head>
-    <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    </head>
-<body>
-    <div class="container">
-        <div class="col-lg-4 col-md-4">
-        
-        </div>
-           <div class="col-lg-4 col-md-4 well" style="margin-top:20%;">
-            <form action="" class="form" method="post">
-  <div class="form-group">
-      <input type="text" name="fn" placeholder="Fullname" class="form-control" required /><br/>
-    </div>
-     
- <div class="form-group">
-      <input type="text" name="un" placeholder="Username" class="form-control" required /><br/>
-    </div>
-               
-     
-<div class="form-group">
-    <input type="password" name="pass" placeholder="Password" class="form-control" required/><br/>
-    </div>
-<div class="form-group">
-    <input type="password" name="cpass" placeholder="Confirm_Password" class="form-control" required/><br/>
-    </div>
-<div class="form-group">
-    <input type="number" name="en1" placeholder="Emergency_no1" class="form-control" required/><br/>
-    </div>
- <div class="form-group">
-    <input type="number" name="en2" placeholder="Emergency_no2" class="form-control" required/><br/>
-    </div>
-<div class="form-group">
-    <input type="number" name="en3" placeholder="Emergency_no3" class="form-control" required/><br/>
-    </div>
-<div class="form-group">
-    <input type="number" name="en4" placeholder="Emergency_no4" class="form-control" required/><br/>
-    </div>
-
-
-   
-     <div class="form-group">
-     <input type="submit" name="submit" class="btn btn-primary pull-right" value="submit"/> 
-        </div>
-</form>
-    </div>
-  
     
-   
- 
-    </div>
-   
-</body>
-
-</html>
